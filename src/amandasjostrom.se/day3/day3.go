5a340a142e367532b9312ae4ffde08d9a7013df0
amandasjostrom <amandasjostrom@hotmail.com>
2018-12-04 08:29:42 +0100

    alternate (slower) version of day 3

--- src/amandasjostrom.se/day3/day3.go
+++ src/amandasjostrom.se/day3/day3.go
@@ -6,18 +6,25 @@ import (
 	"strconv"
 )
 
+var overlappingClaims = map[string]bool{}
+
+type Square struct {
+	count int
+	firstId string
+}
+
 func Run(input []string) common.Result {
 	claims := claims(input)
 	fabric := markClaimsOnFabric(claims)
 	squareInchesUsedByMultipleClaims := squareInchesUsedByMultipleClaims(fabric)
 	fmt.Println("DAY 3, PART 1: ", squareInchesUsedByMultipleClaims)
-	partTwoId := idOfNonOverlappingClaim(claims, fabric)
+	partTwoId := idOfNonOverlappingClaim(claims)
 	fmt.Println("DAY 3, PART 2: ", partTwoId)
 	return common.Result{squareInchesUsedByMultipleClaims, partTwoId}
 }
 
-func markClaimsOnFabric(claims []Claim) map[string]int {
-	var fabric = make(map[string]int)
+func markClaimsOnFabric(claims []Claim) map[string]Square {
+	var fabric = make(map[string]Square)
 	for _, claim := range claims {
 		for x := 0; x < claim.width; x++ {
 			for y := 0; y < claim.height; y++ {
@@ -28,41 +35,36 @@ func markClaimsOnFabric(claims []Claim) map[string]int {
 	return fabric
 }
 
-func markSquareOnFabric(claim Claim, x int, y int, fabric map[string]int) {
+func markSquareOnFabric(claim Claim, x int, y int, fabric map[string]Square) {
 	currentX := claim.startX + x
 	currentY := claim.startY + y
-	fabric[key(currentX, currentY)]++
+	key := key(currentX, currentY)
+	if 	square := fabric[key]; square.firstId != "" {
+		overlappingClaims[square.firstId] = true
+		overlappingClaims[claim.id] = true
+		square.count++
+		fabric[key] = square
+	} else {
+		fabric[key] = Square{1, claim.id}
+	}
 }
 
-func squareInchesUsedByMultipleClaims(fabric map[string]int) (sum int) {
+func squareInchesUsedByMultipleClaims(fabric map[string]Square) (sum int) {
 	for _, v := range fabric {
-		if v > 1 {
+		if v.count > 1 {
 			sum++
 		}
 	}
 	return
 }
 
-func idOfNonOverlappingClaim(claims []Claim, fabric map[string]int) string {
+func idOfNonOverlappingClaim(claims []Claim) string {
 	for _, claim := range claims {
-		if !isOverLapping(claim, fabric) {
+		if !overlappingClaims[claim.id] {
 			return claim.id
 		}
 	}
 	return "ERROR"
-}
-
-func isOverLapping(claim Claim, fabric map[string]int) bool {
-	for x := 0; x < claim.width; x++ {
-		for y := 0; y < claim.height; y++ {
-			currentX := claim.startX + x
-			currentY := claim.startY + y
-			if fabric[key(currentX, currentY)] > 1 {
-				return true
-			}
-		}
-	}
-	return false
 }
 
 func key(x int, y int) string {
--- src/amandasjostrom.se/day3/day3_test.go
+++ src/amandasjostrom.se/day3/day3_test.go
@@ -37,51 +37,31 @@ func Test_markClaimsOnFabric(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want map[string]int
+		want map[string]Square
 	}{
-		{"Handles first", args{[]Claim{{"id", 1, 2, 1, 1}}}, map[string]int{"1+2": 1}},
+		{"Handles first", args{[]Claim{
+			{"id", 1, 2, 1, 1},
+		}}, map[string]Square{"1+2": {1, "id"}}},
+
 		{"Handles second", args{[]Claim{
-			{"id", 1, 2, 1, 1},
-			{"id", 1, 2, 1, 1},
-		}}, map[string]int{"1+2": 2}},
+			{"id1", 1, 2, 1, 1},
+			{"id2", 1, 2, 1, 1},
+		}}, map[string]Square{"1+2": {2, "id1"}}},
+
 		{"Handles overlapping areas", args{[]Claim{
-			{"id", 1, 2, 2, 2},
-			{"id", 1, 2, 1, 1},
-		}}, map[string]int{
-			"1+2": 2,
-			"2+2": 1,
-			"1+3": 1,
-			"2+3": 1,
+			{"id1", 1, 2, 2, 2},
+			{"id2", 1, 2, 1, 1},
+		}}, map[string]Square{
+			"1+2": {2, "id1"},
+			"2+2": {1, "id1"},
+			"1+3": {1, "id1"},
+			"2+3": {1, "id1"},
 		}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := markClaimsOnFabric(tt.args.claims); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("markClaimsOnFabric() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_isOverLapping(t *testing.T) {
-	type args struct {
-		claim  Claim
-		fabric map[string]int
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{"not overlapping 1x1", args{Claim{"id", 1,1,1,1}, map[string]int{"1+1":1}}, false },
-		{"not overlapping 2x2", args{Claim{"id", 1,1,2,2}, map[string]int{"1+1":1, "1+2":1, "2+2":1, "2+1":1}}, false  },
-		{"is overlapping 1x1", args{Claim{"id", 1,1,1,1}, map[string]int{"1+1":2}}, true  },
-		{"is overlapping 2x2", args{Claim{"id", 1,1,2,2}, map[string]int{"1+1":2, "1+2":1, "2+2":1, "2+1":1}}, true  },
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := isOverLapping(tt.args.claim, tt.args.fabric); got != tt.want {
-				t.Errorf("isOverLapping() = %v, want %v", got, tt.want)
 			}
 		})
 	}
