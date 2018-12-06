e28f7e24fa5a3aadb9e08a42cdeeeed2e8ec50bb
amandasjostrom <amandasjostrom@hotmail.com>
2018-12-06 19:48:32 +0100

    template day

---
+++ src/amandasjostrom.se/template/template.go
@@ -0,0 +1,9 @@
+package template
+
+import (
+	"amandasjostrom.se/common"
+)
+
+func Run(input []string) common.Result {
+	return common.Result{0,0}
+}
---
+++ src/amandasjostrom.se/template/template_test.go
@@ -0,0 +1,10 @@
+package template
+
+import (
+	"testing"
+	"amandasjostrom.se/common"
+)
+
+func TestRun(t *testing.T) {
+	common.TestRun(t,"\n", Run,5928, "bqlporuexkwzyabnmgjqctvfs")
+}
